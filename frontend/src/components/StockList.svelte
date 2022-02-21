35c8cf1ae56001fc6178deadec9227e232f7ac72
James McArthur <77163783+jamezmca@users.noreply.github.com>
2022-02-21 11:05:39 -0800

    nice

--- frontend/src/components/StockList.svelte
+++ frontend/src/components/StockList.svelte
@@ -1,11 +1,12 @@
 <script>
+    import StockRow from "./StockRow.svelte";
 	export let stocks;
-	console.log(stocks);
 </script>
 
 <h1>Popular stocks</h1>
 <div>
 	{#each stocks as stock}
 		<h3>{Object.keys(stock)[0]}</h3>
+        <StockRow {stock}/>
 	{/each}
 </div>
---
+++ frontend/src/components/StockRow.svelte
@@ -0,0 +1,6 @@
+<script>
+	export let stock;
+    console.log(stock)
+</script>
+
+<h1>dope</h1>
