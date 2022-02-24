5b2d8d395a50880d4a5eb6bb950a1a5f78def0d0
James McArthur <77163783+jamezmca@users.noreply.github.com>
2022-02-24 08:16:44 -0800

    Update StockList.svelte

--- frontend/src/components/StockList.svelte
+++ frontend/src/components/StockList.svelte
@@ -1,25 +1,26 @@
 <script>
 	import StockRow from './StockRow.svelte';
 	export let stocks;
-	const stockTickers = ['Stock Ticker', ...stocks.map((stock) => Object.keys(stock)[0])];
 	const rowTitles = stocks.map((stock) => {
 		const [key] = Object.entries(stock);
 		return Object.keys(key[1].financials);
 	})[0];
-	stocks.forEach((stock) => {
+	console.log(rowTitles);
+	['Stock Ticker', ...stocks].forEach((stock) => {
 		console.log(stock);
 	});
 </script>
 
 <div class="max-w-full overflow-scroll flex gap-4">
-	{#each ['Stock Ticker', ...stocks] as stock}
+	{#each [{ 'Stock Ticker': 0 }, ...stocks] as stock}
 		<div class="min-w-fit">
+			{Object.keys(stock)[0]}
 			{#each rowTitles as financial}
 				<div>
-					{#if stock === 'Stock Ticker'}
-						{Object.keys(stock)[0]}
+					{#if Object.keys(stock)[0] === 'Stock Ticker'}
+						{financial}
 					{:else}
-						{Object.values(stock)[0][financial]}
+						{stock[Object.keys(stock)[0]].financials[financial]}
 					{/if}
 				</div>
 			{/each}
